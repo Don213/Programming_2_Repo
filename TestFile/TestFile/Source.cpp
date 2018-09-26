b014deda6694cad3dcccb7627b273efcba7f9fcf
Daniel J <danieldrdj01@gmail.com>
2018-09-26 11:50:52 -0800

    update

---
+++ TestFile/TestFile/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+
+int main()
+{
+	string testString = "hello there";
+
+
+
+
+	cout << testString[0] << endl;
+
+	system("pause");
+	return 0;
+	
+
+
+}
